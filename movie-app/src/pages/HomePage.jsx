5bfaebb3ae032b1490052e661274e46b0498baff
thatkhay <johnkingsleyegeonu@gmail.com>
2023-09-12 02:08:32 +0100

    added the logic to search movies

--- movie-app/src/pages/HomePage.jsx
+++ movie-app/src/pages/HomePage.jsx
@@ -1,108 +1,146 @@
-
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Poster from '../assets/poster.svg'
-import Logo from '../assets/logo.png'
-import hamButton from '../assets/ellipse.png'
-import imbdLogo from '../assets/imbd.png'
-import rottenTomatoesLogo from '../assets/rotten-tomatoes.png'
+import Poster from '../assets/poster.svg';
+import Logo from '../assets/logo.png';
+import hamButton from '../assets/ellipse.png';
+import imbdLogo from '../assets/imbd.png';
+import rottenTomatoesLogo from '../assets/rotten-tomatoes.png';
 import MovieCard from '../components/MovieCard';
 import { Container } from '@mui/material';
 import Footer from '../components/Footer';
 import Spinner from '../components/Spinner';
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
+
 function HomePage() {
-const [posterMovieData, setPosterMovieData] = useState(null)
-const [topMovies, setTopMovies] = useState([])
-const [Loading, setLoading] = useState(true)
+  const [posterMovieData, setPosterMovieData] = useState(null);
+  const [topMovies, setTopMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
+  useEffect(() => {
+    const fetchPoster = async () => {
+      try {
+        const apiKey = '7a529b24ef789e4a50de476f2a2bbd35';
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/search/movie?query=John+Wick&api_key=${apiKey}`
+        );
 
-    useEffect(()=> {
-        const fetchPoster = async () => {
-            try {
-                const apiKey = '7a529b24ef789e4a50de476f2a2bbd35'
-                const response = await axios.get(
-                    `https://api.themoviedb.org/3/search/movie?query=John+Wick&api_key=${apiKey}`
-                
-                )
-                
-                // console.log(response.data);
-                setPosterMovieData(response.data.results[3])
-                setLoading(false)
-            } catch (error) {
-                console.log(error);
-                setLoading(false)
-            }
-        }
-        fetchPoster()
-    }, [])
+        setPosterMovieData(response.data.results[3]);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching poster:', error);
+        setLoading(false);
+      }
+    };
+    fetchPoster();
+  }, []);
 
-    useEffect(() => {
-      const fetchTopMovies = async () => {
-        try {
-          const apiKey = '7a529b24ef789e4a50de476f2a2bbd35';
-          const response = await axios.get(
-            `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&page=1`,
-            
-          );
-          const top10Movies = response.data.results.slice(0, 10);
-          console.log(top10Movies);
-          setTopMovies(top10Movies);
-          // toast.success('Top 10 movies fetched successfully.')
-        } catch (error) {
-          console.error('Error fetching top movies:', error);
-          toast.error('An error occurred , try again later.');
-          
-        }
-      };
-      
-      fetchTopMovies();
-    }, []);
-    
+  useEffect(() => {
+    const fetchTopMovies = async () => {
+      try {
+        const apiKey = '7a529b24ef789e4a50de476f2a2bbd35';
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&page=1`
+        );
+
+        const top10Movies = response.data.results.slice(0, 10);
+        setTopMovies(top10Movies);
+        // toast.success('Top 10 movies fetched successfully.')
+      } catch (error) {
+        console.error('Error fetching top movies:', error);
+        toast.error('An error occurred , try again later.');
+      }
+    };
+
+    fetchTopMovies();
+  }, []);
+
+  const handleSearch = async () => {
+    setLoading(true);
+    try {
+      const apiKey = '7a529b24ef789e4a50de476f2a2bbd35';
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&api_key=${apiKey}`
+      );
+      setSearchResults(response.data.results);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error searching for movies:', error);
+      toast.error('An error occurred while searching for movies.');
+    }
+    setLoading(false);
+  };
+
   return (
-    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
-       <div style={{width: '100%', height: '25rem' ,  backgroundImage: `url(${Poster})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
-       <header>
-        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '1rem 5rem 5rem 5rem'}}>
-          <img src={Logo} alt="" />
-          <input type="text" placeholder='What do you want to watch?' style={{border: '3px solid white', backgroundColor: 'transparent',  width: "45rem", borderRadius: '1rem', height: '1.5rem', padding: '0.5rem 1rem', color: 'white' , textTransform: 'capitalize'}}/>
-          <img src={hamButton} alt="" />
-        </div>
-        { posterMovieData ? (
-          <Container>
-           <h2 style={{color: 'white', textAlign: 'left', width: '20rem', paddingLeft: '5rem'}}>{posterMovieData.title}</h2>
-           <div style={{display: 'flex', width:'13rem', justifyContent: 'space-between', paddingLeft: '5rem'}}>
-            <img src={imbdLogo} alt="" />
-            <img src={rottenTomatoesLogo} alt="" />
-           </div>
-        <p style={{color: 'white', textAlign: 'left',  width: '20rem', fontSize:'.7rem', paddingLeft: '5rem'}}>{posterMovieData.overview}</p>
-        </Container>
-        ) 
-        :
-        (
-          <p style={{color: 'white'}}>loading....</p>
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+      <div style={{ width: '100%', height: '25rem', backgroundImage: `url(${Poster})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+        <header>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '1rem 5rem 5rem 5rem' }}>
+            <img src={Logo} alt="" />
+            <input
+              type="text"
+              placeholder="What do you want to watch?"
+              style={{
+                border: '3px solid white',
+                backgroundColor: 'transparent',
+                width: '45rem',
+                borderRadius: '1rem',
+                height: '1.5rem',
+                padding: '0.5rem 1rem',
+                color: 'white',
+                textTransform: 'capitalize'
+              }}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              value={searchQuery}
+              onInput={ handleSearch}
+            />
+            <img src={hamButton} alt="" />
+          </div>
+          {posterMovieData ? (
+            <Container>
+              <h2 style={{ color: 'white', textAlign: 'left', width: '20rem', paddingLeft: '5rem' }}>{posterMovieData.title}</h2>
+              <div style={{ display: 'flex', width: '13rem', justifyContent: 'space-between', paddingLeft: '5rem' }}>
+                <img src={imbdLogo} alt="" />
+                <img src={rottenTomatoesLogo} alt="" />
+              </div>
+              <p style={{ color: 'white', textAlign: 'left', width: '20rem', fontSize: '.7rem', paddingLeft: '5rem' }}>{posterMovieData.overview}</p>
+            </Container>
+          ) : (
+            <p style={{ color: 'white' }}>Loading....</p>
+          )}
+        </header>
+        <main style={{ marginTop: '5rem' }}>
+  <Container>
+    {loading ? (
+      <Spinner />
+    ) : (
+      <>
+        {searchQuery && searchResults.length > 0 && (
+          <div style={{marginBottom: '3rem'}}>
+            <h1 style={{ textAlign: 'left', fontSize: '1.2rem', marginBottom: '2rem' }}>Search Results</h1>
+            <Container className="movie-grid" style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '3rem' }}>
+              {searchResults.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </Container>
+          </div>
         )}
-       
-       </header>
-       <main style={{marginTop: '5rem'}}>
-       <Container >
-      <h1 style={{textAlign: 'left', fontSize: '1.2rem', marginBottom: '1rem'}}>Top 10 Movies</h1>
-      {Loading ? 
-       (<Spinner/>) :
-       (<Container style={{display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '3rem'}}>
-       {topMovies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-       ))}
-     </Container>
-   )}
-   </Container>
-      
-       </main>
-       <Footer/>
-       </div>
-       
+        <h1 style={{ textAlign: 'left', fontSize: '1.2rem', marginBottom: '1rem' }}>Top 10 Movies</h1>
+        <Container style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '3rem' }}>
+          {topMovies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </Container>
+      </>
+    )}
+  </Container>
+</main>
+
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
